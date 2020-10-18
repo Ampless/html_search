a09ebbaa129e056ae365b96bf2e0dcc41366f663
Chris <chrissx@chrissx.de>
2020-10-18 11:43:53 +0200

    did all the code n stuff

---
+++ lib/html_search.dart
@@ -0,0 +1,30 @@
+import 'package:html/dom.dart' as dom;
+import 'package:html/parser.dart';
+
+dom.Element htmlSearchByPredicate(
+  List<dom.Element> rootNode,
+  bool Function(dom.Element) predicate,
+) {
+  for (final e in rootNode) {
+    if (predicate(e)) {
+      return e;
+    }
+    final found = htmlSearchByPredicate(e.children, predicate);
+    if (found != null) {
+      return found;
+    }
+  }
+  return null;
+}
+
+dom.Element htmlSearchByClass(List<dom.Element> rootNode, String className) =>
+    htmlSearchByPredicate(rootNode, (e) => e.className.contains(className));
+
+List<dom.Element> htmlParse(String raw) {
+  final parser = HtmlParser(raw);
+  final result = parser.parse();
+  if (parser.errors != null && parser.errors.isNotEmpty) {
+    throw parser.errors;
+  }
+  return result.children;
+}
